12ab725a580e54396d62c1766b27d89fa7636a5f
preetesh33 <preetesh_sharma@hotmail.com>
2018-09-25 11:33:33 +0530

    Create findhost.sh

---
+++ findhost.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+########
+#owner: Preetesh Sharma
+########
+if [ -z $1 ]; then
+
+echo "No argument provided. Enter a non FQDN host."
+echo
+echo "Usage: $0 <HOST>"
+echo "eg: $0 "example"
+exit
+
+fi
+domains="google.com yahoo.com <http://yahoo.comgmail.com> gmail.com <http://yahoo.comgmail.com> "
+echo -e "Searching for the host $1 in domains:\n\n$domains\n"
+for domain in $domains; do
+result=$(host -t a $1.${domain})
+if [ $? == 0 ]; then
+echo $result
+hostfound="TRUE"
+fi
+done
+
+if [ "$hostfound" != "TRUE" ]; then
+echo "Host not found"
+fi
