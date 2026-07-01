94afd178c16734503bf2d91b42b3ea12dcf19efb
preetesh33 <preetesh_sharma@hotmail.com>
2026-07-01 19:55:46 +0530

    Update rpms_list.sh

--- rpms_list.sh
+++ rpms_list.sh
@@ -9,7 +9,7 @@ while IFS= read -r var
 
 do
 salt $var cmd.run "echo $var > /tmp/$var.txt && cat /etc/redhat-release >> /tmp/$var.txt && rpm -qa >> /tmp/$var.txt"
-salt $var cmd.run "curl --insecure --user root:J@ckp0t5 -T /tmp/$var.txt sftp://10.173.96.207/home/prsharma/hostlist/"
+salt $var cmd.run "curl --insecure --user root:J@ckp0t5 -T /tmp/$var.txt sftp://x.x.x.x/home/prsharma/hostlist/"
 done < "$input"
 
 
